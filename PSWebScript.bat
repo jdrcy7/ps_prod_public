c7b7208c83fbd999033399bf50f917cefb507b24
JDR <119295162+jdrcy7@users.noreply.github.com>
2022-11-28 15:38:54 +0000

    Update PSWebScript.bat

--- PSWebScript.bat
+++ PSWebScript.bat
@@ -2,13 +2,11 @@
 set SOURCE="{WebScript}"
 net session >nul 2>&1
 if %errorLevel% == 0 (
-	wmic process where (Name="WMIC.exe" AND CommandLine LIKE "%%%TIME%%%") get ParentProcessId /value | find "ParentProcessId" >%T%
-	set /P A=<%T%
-	set PID=%A:~16%
+	set RAND=%RANDOM%%RANDOM%
 	powershell Set-ExecutionPolicy bypass
-	powershell Invoke-WebRequest -Uri "%SOURCE%" -OutFile "%SYSTEMROOT%\Temp\PSWebScript_%PID%.ps1"
-	powershell "%SYSTEMROOT%\Temp\PSWebScript_%PID%.ps1"
-	powershell Remove-Item "%SYSTEMROOT%\Temp\PSWebScript_%PID%.ps1" -Force
+	powershell Invoke-WebRequest -Uri "%SOURCE%" -OutFile "%SYSTEMROOT%\Temp\PSWebScript_%RAND%.ps1"
+	powershell "%SYSTEMROOT%\Temp\PSWebScript_%RAND%.ps1"
+	powershell Remove-Item "%SYSTEMROOT%\Temp\PSWebScript_%RAND%.ps1" -Force
 	powershell Set-ExecutionPolicy restricted
 ) else (
     echo Error: elevated permissions recquired to initiate script.
