d2bc6aef302af1dabaf0f50fd38e239cedf0ebbd
JDR <119295162+jdrcy7@users.noreply.github.com>
2022-11-28 11:56:19 +0000

    PowerShell WebScript Loader
    Set {WebScript} to raw script source

---
+++ PSWebScript.bat
@@ -0,0 +1,13 @@
+@echo off
+set SOURCE="{WebScript}"
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    powershell Set-ExecutionPolicy bypass
+	powershell Invoke-WebRequest -Uri "%SOURCE%" -OutFile "%SYSTEMROOT%\Temp\PSWebScript.ps1"
+	powershell "%SYSTEMROOT%\Temp\PSWebScript.ps1"
+	powershell Remove-Item "%SYSTEMROOT%\Temp\PSWebScript.ps1" -Force
+	powershell Set-ExecutionPolicy restricted
+) else (
+    echo Error: elevated permissions recquired to initiate script.
+)
+exit /b %errorLevel%
