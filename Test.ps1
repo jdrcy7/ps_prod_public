e548e5f169ac5325ee0653a2fe9f6bf1118ff6e4
JDR <119295162+jdrcy7@users.noreply.github.com>
2022-11-28 15:59:34 +0000

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,4 +1,4 @@
-$host.UI.RawUI.WindowTitle = "Title Test"
+$host.UI.RawUI.WindowTitle = "Title Test with Jane"
 Write-Host "Green" -ForegroundColor Green
 Write-Host "Red" -ForegroundColor Red
 Write-Host "Blue" -ForegroundColor Blue
