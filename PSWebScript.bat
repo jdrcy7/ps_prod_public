cac77f2195e097ca6ade8e543456396dea4e6b59
JDR <119295162+jdrcy7@users.noreply.github.com>
2022-11-28 15:22:53 +0000

    Update PSWebScript.bat

--- PSWebScript.bat
+++ PSWebScript.bat
@@ -2,10 +2,11 @@
 set SOURCE="{WebScript}"
 net session >nul 2>&1
 if %errorLevel% == 0 (
-    powershell Set-ExecutionPolicy bypass
-	powershell Invoke-WebRequest -Uri "%SOURCE%" -OutFile "%SYSTEMROOT%\Temp\PSWebScript_%random%%random%.ps1"
-	powershell "%SYSTEMROOT%\Temp\PSWebScript_%random%%random%.ps1"
-	powershell Remove-Item "%SYSTEMROOT%\Temp\PSWebScript_%random%%random%.ps1" -Force
+	set %rand%=%random%%random%
+	powershell Set-ExecutionPolicy bypass
+	powershell Invoke-WebRequest -Uri "%SOURCE%" -OutFile "%SYSTEMROOT%\Temp\PSWebScript_%rand%.ps1"
+	powershell "%SYSTEMROOT%\Temp\PSWebScript_%rand%.ps1"
+	powershell Remove-Item "%SYSTEMROOT%\Temp\PSWebScript_%rand%.ps1" -Force
 	powershell Set-ExecutionPolicy restricted
 ) else (
     echo Error: elevated permissions recquired to initiate script.
